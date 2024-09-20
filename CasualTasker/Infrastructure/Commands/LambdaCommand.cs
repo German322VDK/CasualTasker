c7a9998b51ea6d7535eb3a6f2022b5f9a1263c8c
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-21 00:03:53 +1000

    Добавление комманд

---
+++ CasualTasker/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using CasualTasker.Infrastructure.Commands.Base;
+
+namespace CasualTasker.Infrastructure.Commands
+{
+    public class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+
+        private readonly Predicate<object> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Predicate<object> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        protected override bool CanExecute(object p) => _CanExecute?.Invoke(p) ?? true;
+
+        protected override void Execute(object p) => _Execute(p);
+    }
+}
