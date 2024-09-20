07593809d2a8f202dadb3fdc73b8b452486498df
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-20 23:48:11 +1000

    класс синхронизирующий ObservableCategoryDbCollection и ObservableTaskDbCollection при удалении/изменении категории в представлении

---
+++ CasualTasker/Infrastructure/ObservableDbCollections/CategoryTaskSynchronizer.cs
@@ -0,0 +1,47 @@
+﻿using CasualTasker.DTO;
+
+namespace CasualTasker.Infrastructure.ObservableDbCollections
+{
+    public class CategoryTaskSynchronizer
+    {
+        private readonly IObservableDbCollection<TaskDTO> _tasks;
+        private readonly ObservableCategoryDbCollection _categories;
+        public CategoryTaskSynchronizer(IObservableDbCollection<TaskDTO> tasks, ObservableCategoryDbCollection categories)
+        {
+            _tasks = tasks;
+            _categories = categories;
+        }
+
+        public void Synchronize()
+        {
+            _categories.OnCategoryUpdated += HandleCategoryUpdated;
+            _categories.OnCategoryDeleted += HandleCategoryDeleted;
+        }
+
+        private void HandleCategoryDeleted(CategoryDTO deletedCategory)
+        {
+            var tasksToUpdate = _tasks.Entities
+               .Where(task => task.Category.Id == deletedCategory.Id)
+               .ToList();
+
+            CategoryDTO defaultCategory = _categories.DefaultDeletedEntity;
+
+            foreach (var task in tasksToUpdate)
+            {
+                task.Category = defaultCategory;
+                _tasks.Update(task, false);
+            }
+        }
+
+        private void HandleCategoryUpdated(CategoryDTO updatedCategory)
+        {
+            var tasksToUpdate = _tasks.Entities.Where(task => task.Category.Id == updatedCategory.Id);
+
+            foreach (var task in tasksToUpdate)
+            {
+                task.Category = updatedCategory;
+                _tasks.Update(task, false);
+            }
+        }
+    }
+}
