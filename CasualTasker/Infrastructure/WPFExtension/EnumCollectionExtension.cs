b4778fd37c8dbd81de49438788cda4315bd24a41
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-20 22:36:06 +1000

    Enum to Xaml

---
+++ CasualTasker/Infrastructure/WPFExtension/EnumCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Markup;
+
+namespace CasualTasker.Infrastructure.WPFExtension
+{
+    public class EnumCollectionExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumCollectionExtension(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+                throw new ArgumentNullException(nameof(enumType), "Type must not be null and be enum");
+
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (EnumType != null)
+            {
+                return CreateEnumValueList(EnumType);
+            }
+            return default;
+        }
+
+        private List<object> CreateEnumValueList(Type enumType)
+        {
+            return Enum.GetNames(enumType)
+                .Select(name => Enum.Parse(enumType, name))
+                .ToList();
+        }
+    }
+}
