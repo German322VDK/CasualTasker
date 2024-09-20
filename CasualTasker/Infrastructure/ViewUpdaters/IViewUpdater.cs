a778295df0a62c00ceebcaf61bbf09b5f8377693
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-20 23:17:11 +1000

    класс ViewUpdater для работы с ObservableCollection

---
+++ CasualTasker/Infrastructure/ViewUpdaters/IViewUpdater.cs
@@ -0,0 +1,18 @@
+﻿using CasualTasker.DTO.Base;
+using System.ComponentModel;
+
+namespace CasualTasker.Infrastructure.ViewUpdaters
+{
+    public interface IViewUpdater<TEntity> where TEntity : NamedEntity
+    {
+        TEntity First { get; }
+        ICollectionView ViewEntities { get; }
+        IEnumerable<TEntity> Entities { get; }
+        void Add(TEntity entity);
+        TEntity Get(int id);
+        TEntity Get(TEntity entity);
+        void Delete(int index);
+        void Update(TEntity entity);
+        void UpdateFromDB(IEnumerable<TEntity> data);
+    }
+}
---
+++ CasualTasker/Infrastructure/ViewUpdaters/ViewUpdater.cs
@@ -0,0 +1,57 @@
+﻿using CasualTasker.DTO.Base;
+using CasualTasker.Infrastructure.WPFExtension;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace CasualTasker.Infrastructure.ViewUpdaters
+{
+    public class ViewUpdater<TEntity> : IViewUpdater<TEntity> where TEntity : NamedEntity
+    {
+        protected readonly ObservableCollection<TEntity> _observableEntities;
+
+        public ViewUpdater(ObservableCollection<TEntity> observableEntities)
+        {
+            _observableEntities = observableEntities;
+        }
+
+        public TEntity First => _observableEntities.FirstOrDefault();
+        public ICollectionView ViewEntities =>
+            CollectionViewSource.GetDefaultView(_observableEntities);
+        public IEnumerable<TEntity> Entities => _observableEntities;
+
+        public void Add(TEntity entity)
+        {
+            _observableEntities.Add(entity);
+            NotifyUpdated();
+        }
+
+        public void Delete(int id)
+        {
+            var index = _observableEntities.FindItem(id);
+            _observableEntities.RemoveAt(index);
+            NotifyUpdated();
+        }
+
+        public TEntity Get(int id) =>
+            _observableEntities.FirstOrDefault(el => el.Id == id);
+        public TEntity Get(TEntity entity) =>
+           _observableEntities.FirstOrDefault(el => el.Id == entity.Id);
+
+        public void Update(TEntity entity)
+        {
+            var index = _observableEntities.FindItem(entity.Id);
+            _observableEntities[index].UpdateFrom(entity);
+            NotifyUpdated();
+        }
+
+        public void UpdateFromDB(IEnumerable<TEntity> data)
+        {
+            _observableEntities.AddRangeWithClear(data);
+            NotifyUpdated();
+        }
+
+        private void NotifyUpdated() =>
+            _observableEntities.UpdateView();
+    }
+}
