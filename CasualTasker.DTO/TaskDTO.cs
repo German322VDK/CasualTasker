355fdb6b0fb20185b1486fd0ba6ab2b41414e58c
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-20 23:02:15 +1000

    add TasKDTO

---
+++ CasualTasker.DTO/TaskDTO.cs
@@ -0,0 +1,47 @@
+﻿using CasualTasker.DTO.Base;
+
+namespace CasualTasker.DTO
+{
+    public class TaskDTO : NamedEntity
+    {
+        public string Description { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; } = DateTime.Now;
+        public CasualTaskStatus Status { get; set; } = CasualTaskStatus.InProgress;
+
+        public virtual CategoryDTO Category { get; set; }
+
+        public override object Clone() =>
+            new TaskDTO
+            {
+                Id = Id,
+                Description = Description,
+                Status = Status,
+                Category = Category.Clone() as CategoryDTO,
+                DueDate = DueDate,
+                Name = Name,
+            };
+
+        public override void UpdateFrom(NamedEntity entity)
+        {
+            base.UpdateFrom(entity);
+            if (entity is TaskDTO task)
+            {
+                Description = task.Description;
+                Status = task.Status;
+                DueDate = task.DueDate;
+                Category = task.Category.Clone() as CategoryDTO;
+            }
+            else
+            {
+                throw new ArgumentException($"entity.Type: {entity.GetType().Name} not equal to type:{nameof(TaskDTO)}");
+            }
+        }
+    }
+
+    public enum CasualTaskStatus
+    {
+        InProgress = 1,
+        Completed = 2,
+        Postponed = 3,
+    }
+}
