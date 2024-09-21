d83c205b18a97ccf54faf4d851d026e6400fb085
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-21 17:07:26 +1000

    CasualTaskerDbContext - контекст БД

---
+++ CasualTasker.Database/Context/CasualTaskerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CasualTasker.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasualTasker.Database.Context
+{
+    public class CasualTaskerDbContext : DbContext
+    {
+        public DbSet<TaskDTO> Tasks { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
+        public CasualTaskerDbContext(DbContextOptions<CasualTaskerDbContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+    }
+}
