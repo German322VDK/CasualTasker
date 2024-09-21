0a167ab236409653adcea28d3884ab2ab89e4c5a
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-21 18:59:23 +1000

    Внедрение ISotre в ObservableDbCollections

--- CasualTasker/Infrastructure/ObservableDbCollections/ObservableTaskDbCollection.cs
+++ CasualTasker/Infrastructure/ObservableDbCollections/ObservableTaskDbCollection.cs
@@ -1,16 +1,12 @@
-﻿using CasualTasker.Database.StaticData;
-using CasualTasker.DTO;
-using CasualTasker.Infrastructure.ViewUpdaters;
+﻿using CasualTasker.DTO;
+using CasualTasker.Services.Stores;
 using Microsoft.Extensions.Logging;
-using System.Collections.ObjectModel;
 
 namespace CasualTasker.Infrastructure.ObservableDbCollections
 {
     public class ObservableTaskDbCollection : ObservableDbCollection<TaskDTO>
     {
-        public ObservableTaskDbCollection(ILogger<ObservableDbCollection<TaskDTO>> logger) : base(logger)
-        {
-            _viewUpdater = new ViewUpdater<TaskDTO>(new ObservableCollection<TaskDTO>(TestData.Tasks));
-        }
+        public ObservableTaskDbCollection(IStore<TaskDTO> store, ILogger<ObservableDbCollection<TaskDTO>> logger)
+             : base(store, logger) { }
     }
 }
