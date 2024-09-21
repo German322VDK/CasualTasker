d6b57f769942bf75947f3c40a1ac36724675f480
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-21 17:08:15 +1000

    CasualTaskerDesignTimeDbContextFactory класс собирающий SQlite Db Code first

---
+++ CasualTasker.Database/Context/DesignTimeDbContextFactories/Sqlite/CasualTaskerDesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CasualTasker.Database.Context.DesignTimeDbContextFactories.Sqlite
+{
+    public class CasualTaskerDesignTimeDbContextFactory : IDesignTimeDbContextFactory<CasualTaskerDbContext>
+    {
+        public CasualTaskerDbContext CreateDbContext(string[] args)
+        {
+            const string connection_string = "Data Source=CasualTaskManager.db;Cache=Shared";
+
+            DbContextOptionsBuilder<CasualTaskerDbContext> optionsBuilder = new DbContextOptionsBuilder<CasualTaskerDbContext>();
+            optionsBuilder.UseSqlite(connection_string);
+
+            return new CasualTaskerDbContext(optionsBuilder.Options);
+        }
+    }
+}
