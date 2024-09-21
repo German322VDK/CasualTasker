2e2d15e8aadcd8ee12e5b4afd5c427b9c8a88ed9
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-21 17:38:15 +1000

    Интерфейсы IStore, IStoreSync, IStoreAsync

---
+++ CasualTasker.Services/Stores/IStore.cs
@@ -0,0 +1,24 @@
+﻿using CasualTasker.DTO.Base;
+
+namespace CasualTasker.Services.Stores
+{
+    public interface IStore<TEntity> : IStoreSync<TEntity>, IStoreAsync<TEntity> where TEntity : NamedEntity
+    {
+        IQueryable<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+
+    public interface IStoreSync<TEntity> where TEntity : NamedEntity
+    {
+        TEntity Add(TEntity item);
+        TEntity Update(TEntity item);
+        bool Delete(int id);
+    }
+
+    public interface IStoreAsync<TEntity> where TEntity : NamedEntity
+    {
+        Task<TEntity> AddAsync(TEntity item);
+        Task<TEntity> UpdateAsync(TEntity item);
+        Task<bool> DeleteAsync(int id);
+    }
+}
