e33a9f519e615681d6e35893a47d408f301777c0
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-22 23:10:31 +1000

    Документация контекста БД

--- CasualTasker.Database/Context/DesignTimeDbContextFactories/Sqlite/CasualTaskerDesignTimeSqliteDbContextFactory.cs
+++ CasualTasker.Database/Context/DesignTimeDbContextFactories/Sqlite/CasualTaskerDesignTimeSqliteDbContextFactory.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace CasualTasker.Database.Context.DesignTimeDbContextFactories.Sqlite
 {
+    /// <summary>
+    /// Provides a factory for creating instances of <see cref="CasualTaskerDbContext"/> at design time for SQLite database.
+    /// This factory is typically used by tools like Entity Framework migrations when the application is not running.
+    /// </summary>
     public class CasualTaskerDesignTimeSqliteDbContextFactory : IDesignTimeDbContextFactory<CasualTaskerDbContext>
     {
         public CasualTaskerDbContext CreateDbContext(string[] args)
