1674c120853a378d001409aa6f64e72d37990f9b
Герман <65992547+German322VDK@users.noreply.github.com>
2024-09-23 13:15:19 +1000

    Документация WPFExtension

--- CasualTasker/Infrastructure/WPFExtension/ColorToHexConverter.cs
+++ CasualTasker/Infrastructure/WPFExtension/ColorToHexConverter.cs
@@ -4,8 +4,19 @@ using System.Windows.Media;
 
 namespace CasualTasker.Infrastructure.WPFExtension
 {
+    /// <summary>
+    /// Converts a color in hex format to a Color object and vice versa.
+    /// </summary>
     public class ColorToHexConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a hex string to a Color object.
+        /// </summary>
+        /// <param name="value">The hex string to convert.</param>
+        /// <param name="targetType">The target type (not used in this conversion).</param>
+        /// <param name="parameter">Additional parameters (not used).</param>
+        /// <param name="culture">The culture information.</param>
+        /// <returns>A Color object or Transparent if conversion fails.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is string hex && !string.IsNullOrEmpty(hex))
@@ -23,6 +34,14 @@ namespace CasualTasker.Infrastructure.WPFExtension
             return Colors.Transparent;
         }
 
+        /// <summary>
+        /// Converts a Color object back to a hex string.
+        /// </summary>
+        /// <param name="value">The Color object to convert.</param>
+        /// <param name="targetType">The target type (not used in this conversion).</param>
+        /// <param name="parameter">Additional parameters (not used).</param>
+        /// <param name="culture">The culture information.</param>
+        /// <returns>A hex string representing the Color or null if value is not a Color.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is Color color)
--- CasualTasker/Infrastructure/WPFExtension/EnumCollectionExtension.cs
+++ CasualTasker/Infrastructure/WPFExtension/EnumCollectionExtension.cs
@@ -2,10 +2,22 @@
 
 namespace CasualTasker.Infrastructure.WPFExtension
 {
+    /// <summary>
+    /// Provides a markup extension that generates a list of values from a specified enum type.
+    /// This can be used in XAML to bind to enum values, enabling easier access to enum members.
+    /// </summary>
     public class EnumCollectionExtension : MarkupExtension
     {
+        /// <summary>
+        /// Gets the type of the enum from which values are generated.
+        /// </summary>
         public Type EnumType { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumCollectionExtension"/> class.
+        /// </summary>
+        /// <param name="enumType">The type of the enum to generate values from.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="enumType"/> is null or not an enum type.</exception>
         public EnumCollectionExtension(Type enumType)
         {
             if (enumType is null || !enumType.IsEnum)
@@ -14,6 +26,11 @@ namespace CasualTasker.Infrastructure.WPFExtension
             EnumType = enumType;
         }
 
+        /// <summary>
+        /// Provides the generated list of enum values for use in XAML.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider for obtaining services.</param>
+        /// <returns>A list of enum values.</returns>
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             if (EnumType != null)
@@ -23,6 +40,11 @@ namespace CasualTasker.Infrastructure.WPFExtension
             return default;
         }
 
+        /// <summary>
+        /// Creates a list of enum values from the specified enum type.
+        /// </summary>
+        /// <param name="enumType">The type of the enum to create a value list from.</param>
+        /// <returns>A list of enum values.</returns>
         private List<object> CreateEnumValueList(Type enumType)
         {
             return Enum.GetNames(enumType)
